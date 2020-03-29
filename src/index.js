ab019019dff4558d5677f3bb4d275d9452627c79
Chaveztron <jesus.chavez.caro@uabc.edu.mx>
2020-03-28 22:09:47 -0700

    Anadiendo archivos de github

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './App'; //2) importamos la app funcion
 import * as serviceWorker from './serviceWorker';
-
+//3) La funcion renderiza la app funcion en forma de etiqueta html
+                             // 4) remplazando el elemento root
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
